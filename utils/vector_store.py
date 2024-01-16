99cb4f972b8123b16fb14d337020830aa810a18a
kaustav202 <21f2000656@student.onlinedegree.iitm.ac.in>
2024-01-16 18:22:35 +0530

    vectorstore - upsert and connect new index

--- utils/vector_store.py
+++ utils/vector_store.py
@@ -31,3 +31,37 @@ class PineconeWrapper:
 
 
 
+    def connect_index(self, index_name: str):
+
+        indexes = self.pc.list_indexes()
+
+        is_there = False
+
+        for ind in indexes:
+            if index_name == ind.name:
+                is_there = True
+
+        if not is_there:
+            raise ValueError(f"Index '{index_name}' does not exist.")
+        self.index = self.pc.Index(index_name)
+        print(f"Connected to index '{index_name}'.")
+
+
+
+    def upsert_vectors(self, vectors: List[Tuple[str, List[float]]]):
+        """
+        :param vectors: List of tuples where each tuple contains an ID and a vector (list of floats).
+        """
+
+        if not self.index:
+            raise RuntimeError("No index connected. Please connect or create an index first.")
+        self.index.upsert(vectors=vectors)
+        print(f"Upserted {len(vectors)} vectors.")
+
+
+if __name__ == "__main__":
+    pc = PineconeWrapper()
+    pc.create_index("test", 1024)
+    pc.connect_index(index_name="test")
+    vectors = np.random.randint(0, 10, (100, 1024))
+    pc.upsert_vectors(vectors.tolist())
