d07306743b55904df209388f241df12d23344eb7
kaustav202 <21f2000656@student.onlinedegree.iitm.ac.in>
2024-01-22 21:53:24 +0530

    orchestrator init - vectorstore config and retriever setup

---
+++ orchestrator.py
@@ -0,0 +1,71 @@
+from langchain_core.prompts import ChatPromptTemplate
+# from langchain_core.output_parsers import StrOutputParser
+# from langchain_core.runnables import RunnablePassthrough
+from langchain_pinecone import PineconeVectorStore  
+from langchain_voyageai import VoyageAIEmbeddings
+from pinecone import Pinecone, ServerlessSpec
+import numpy as np
+import os
+from uuid import uuid4
+from dotenv import load_dotenv
+load_dotenv()
+from typing import Any, Dict, Iterable
+
+from elasticsearch import Elasticsearch
+from elasticsearch.helpers import bulk
+from langchain_elasticsearch import ElasticsearchRetriever
+
+
+from langchain.retrievers import EnsembleRetriever
+from langchain_community.retrievers import BM25Retriever
+
+
+pinecone_api_key = os.getenv('PINECONE_KEY')
+
+
+# pinecone_wrapper.create_index("vec-store", dimension=1024)
+
+
+pc = Pinecone(api_key=pinecone_api_key)
+
+index = pc.Index("prod-l2")
+
+
+emb_obj = VoyageAIEmbeddings(voyage_api_key=os.getenv('VOYAGE_KEY'), model="voyage-law-2")
+
+
+vector_store = PineconeVectorStore(index=index, embedding=emb_obj)
+
+
+
+
+pinecone_retriever = vector_store.as_retriever(
+    search_type="similarity_score_threshold",
+    search_kwargs={"k": 10, "score_threshold": 0.5}
+)
+
+
+
+       
+es_client = Elasticsearch(['localhost:9200'], basic_auth=('elastic', 'changeme'))
+ 
+es_url = "http://localhost:9200"
+
+def bm25_query(search_query: str) -> Dict:
+    return {
+        "query": {
+            "match": {
+                'content': search_query,
+            },
+        },
+    }
+
+
+bm25_retriever = ElasticsearchRetriever.from_es_params(
+    index_name="prod_l2",
+    body_func=bm25_query,
+    content_field='content',
+    url=es_url,
+    username="elastic",
+    password="changeme"
+)
