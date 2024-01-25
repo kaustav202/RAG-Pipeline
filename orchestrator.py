ca60b1602f0063e1a55ed5b1be1d3ddc6a98bf6c
kaustav202 <21f2000656@student.onlinedegree.iitm.ac.in>
2024-01-25 19:14:36 +0530

    llm init

--- orchestrator.py
+++ orchestrator.py
@@ -94,3 +94,18 @@ ensemble_retriever = EnsembleRetriever(
 )
 
 
+
+
+os.environ["ANTHROPIC_API_KEY"] = os.getenv('ANTHROPIC_KEY')
+
+from langchain_anthropic import ChatAnthropic
+
+llm = ChatAnthropic(
+    model="claude-3-5-sonnet-20240620",
+    temperature=0,
+    max_tokens=1024,
+    timeout=None,
+    max_retries=2,
+)
+
+
