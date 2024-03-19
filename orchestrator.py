3216809533d694bed6266cd46b5d941c2fb84ca9
kaustav202 <21f2000656@student.onlinedegree.iitm.ac.in>
2024-03-19 16:29:38 +0530

    agent prompts

--- orchestrator.py
+++ orchestrator.py
@@ -135,3 +135,59 @@ retriever_tool = create_retriever_tool(
 
 
 # tools = [retriever_tool]
+
+
+system_context = '''You are an intelligent agent.
+Answer the following questions as best you can. 
+You have access to the following tools:
+
+{tools}
+
+You have access to the following tools to select the prompt dynamically based on the query:
+
+{tools}
+
+Use the following format:
+
+Question: the input question you must answer
+Thought: you should always think about what to do
+Action: the action to take, should be one of [{tool_names}]
+Action Input: the input to the action
+Observation: the result of the action
+... (this Thought/Action/Action Input/Observation can repeat N times)
+Thought: I now know the final answer
+Final Answer: the final answer to the original input question
+Begin!
+'''
+
+
+prompt_agent = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            system_context,
+        ),
+        ("human", "{input}"),
+        ("placeholder", "{agent_scratchpad}")
+    ]
+)
+
+agent_sys = '''Answer the following questions as best you can. You have access to the following tools:
+
+{tools}
+
+Use the following format:
+
+Question: the input question you must answer
+Thought: you should always think about what to do
+Action: the action to take, should be one of [{tool_names}]
+Action Input: the input to the action
+Observation: the result of the action
+... (this Thought/Action/Action Input/Observation can repeat N times)
+Thought: I now know the final answer
+Final Answer: the final answer to the original input question
+
+Begin!
+
+Question: {input}
+'''
